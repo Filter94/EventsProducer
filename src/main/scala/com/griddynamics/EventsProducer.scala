c67c4879ad7419eeefb685df02fa8bad7c5e5f2b
Filter94 <IamFilter94@gmail.com>
2018-09-18 16:26:24 +0300

    Minor improvements.

--- src/main/scala/com/griddynamics/EventsProducer.scala
+++ src/main/scala/com/griddynamics/EventsProducer.scala
@@ -9,19 +9,25 @@ import scala.io.BufferedSource
 
 
 object EventsProducer extends App with Logging {
-  lazy val in = new BufferedSource(s.getInputStream).getLines()
-  val eventsGenerator = DefaultEventsGenerator()
-  val s = new Socket(InetAddress.getByName(args(0)), args(1).toInt)
-  val out = new PrintStream(s.getOutputStream)
-  var counter = 0
-  sys.addShutdownHook({
-    logger.info("Written %d events.".format(counter))
-  })
-  do {
-    val eventString = eventsGenerator.next().toString
-    out.println(eventString)
-    logger.debug(eventString)
-    counter += 1
-  } while (in.hasNext && in.next() != null) // to slow things down a bit.
-  s.close()
+  if (args.length == 2) {
+    val eventsGenerator = DefaultEventsGenerator()
+    val (host, port) = (args(0), args(1).toInt)
+    val s = new Socket(InetAddress.getByName(host), port)
+    lazy val in = new BufferedSource(s.getInputStream).getLines()
+    val out = new PrintStream(s.getOutputStream)
+    var counter = 0
+    sys.addShutdownHook({
+      logger.info("Written %d events.".format(counter))
+    })
+    do {
+      val eventString = eventsGenerator.next().toString
+      out.println(eventString)
+      logger.debug(eventString)
+      counter += 1
+    } while (in.hasNext && in.next() != null) // to slow things down a bit.
+    s.close()
+  } else {
+    logger.error("This program accepts 2 arguments. " +
+      "First one is the to which events to be sent, second one is the port.")
+  }
 }
